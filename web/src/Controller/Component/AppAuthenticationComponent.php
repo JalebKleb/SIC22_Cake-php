d61a36e75d2bc5e0c3d09071b83eb663949da2a5
kaledi <kaledihu3@gmail.com>
2022-10-05 16:41:06 -0400

    Primeiro commit

---
+++ web/src/Controller/Component/AppAuthenticationComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Component;
+
+use Authentication\Authenticator\UnauthenticatedException;
+use Authentication\Controller\Component\AuthenticationComponent;
+use Cake\Controller\Component\FlashComponent;
+
+class AppAuthenticationComponent extends AuthenticationComponent
+{
+    public $components = [
+        'Flash'
+    ];
+
+    protected function doIdentityCheck() : void
+    {
+        try{
+            parent::doIdentityCheck();
+        }catch(UnauthenticatedException $exception) {
+            $this->Flash->error("Autenticação necessária !!!");
+
+            throw $exception;
+        }
+    }
+
+}
---
+++ web/src/Controller/PerguntasController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+class PerguntasController extends AppController
+{
+    public function index(){
+        $perguntas = $this->Perguntas
+        ->find()
+        ->contain(['Usuarios', 'Perguntas']);
+
+        $this->paginate = [
+            'contain' => ['Usuarios', 'Respostas']
+        ];
+
+        $perguntas = $this->paginate($this->Perguntas);
+
+        $this->set('perguntas', $perguntas);
+
+        $pergunta = $this->Perguntas->newEmptyEntity();
+
+        $this->set('pergunta', $pergunta);
+    }
+
+    public function add(){
+        $pergunta = $this->Perguntas->newEmptyEntity();
+
+        if($this->request->is('post')){
+            $pergunta = $this->Perguntas->patchEntity($pergunta, $this->request->getData());
+
+            $usuarioLogado = $this->Authentication->getIdentity();
+
+            $pergunta["usuario_id"] = $usuarioLogado->id;
+
+            if($this->Perguntas->save($pergunta)){
+                $this->Flash->success("Pergunta Criada!");
+                return $this->redirect(['action' => 'index']);
+            }
+
+            $this->Flash->error("Erro ao criar Pergunta!");
+        }
+
+        $this->set('pergunta', $pergunta);
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
